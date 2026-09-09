a66e31ebdf367d6ca5f2e4ef63a6b322cf154491
Mandar47 <mandarr47@gmail.com>
2026-09-09 19:02:45 +0000

    labs 6,7, exercise 7

--- 000_Labs/006_doubles/base_code/starter.java
+++ 000_Labs/006_doubles/base_code/starter.java
@@ -9,6 +9,8 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		double celsius = 212.5;
+		double fahrenheight = ((celsius*1.8) + 32);
+		System.out.print("The Fahrenheight value for " + celsius + " degrees Celsius is " + fahrenheight + " degrees.");
 	}
 }
--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -9,6 +9,25 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		String firstname = sc.nextLine();
+		System.out.println("Whats your First Name?");
+		System.out.println("Your Name is " + firstname);
+		int age = sc.nextInt();
+		System.out.println("Whats your age?");
+		System.out.println("Your age is" + age);
+		String birthdaymonth = sc.nextLine();
+		System.out.println("What is your birthday month");
+		System.out.println("Your birthday month is " + birthdaymonth);
+		String birthdayday = sc.nextLine();
+		System.out.println("What is your birthday day?");
+		System.out.println("Your birthday day is " + birthdayday);
+		String birthdayyear = sc.nextLine();
+		System.out.println("What is your birthday year");
+		System.out.println("Your birthday year is " + birthdayyear);
+		double buck50 = sc.nextDouble();
+		System.out.print("What is a Buck Fifty?");
+		System.out.print("A buck fifty is " + buck50);
+
 	}
 }
