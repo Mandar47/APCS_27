2b883a4868334fde86fc8028ac3281dda8052d9a
Mandar47 <mandarr47@gmail.com>
2026-09-10 19:27:43 +0000

    lab 8 lab 9 exercise 8

--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -10,23 +10,24 @@ class starter {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
 		Scanner sc = new Scanner(System.in);
-		String firstname = sc.nextLine();
 		System.out.println("Whats your First Name?");
+		String firstname = sc.nextLine();
 		System.out.println("Your Name is " + firstname);
-		int age = sc.nextInt();
 		System.out.println("Whats your age?");
-		System.out.println("Your age is" + age);
+		int age = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Your age is " + age);
+		System.out.println("What is your birthday month?");
 		String birthdaymonth = sc.nextLine();
-		System.out.println("What is your birthday month");
 		System.out.println("Your birthday month is " + birthdaymonth);
-		String birthdayday = sc.nextLine();
 		System.out.println("What is your birthday day?");
+		int birthdayday = sc.nextInt();
 		System.out.println("Your birthday day is " + birthdayday);
-		String birthdayyear = sc.nextLine();
 		System.out.println("What is your birthday year");
+		int birthdayyear = sc.nextInt();
 		System.out.println("Your birthday year is " + birthdayyear);
-		double buck50 = sc.nextDouble();
 		System.out.print("What is a Buck Fifty?");
+		double buck50 = sc.nextDouble();
 		System.out.print("A buck fifty is " + buck50);
 
 	}
--- 000_Labs/009_math_methods/base_code/starter.java
+++ 000_Labs/009_math_methods/base_code/starter.java
@@ -9,6 +9,14 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		
+		double max1 = Math.max(13 - 6 * 11, 30 % 7 * (-2)); 
+		System.out.println("Maximum = " + max1);
+		double sqrt = Math.sqrt(3 * 8 + 31 % 7);
+		System.out.println("Square root = " + sqrt);
+		double pow = Math.pow(37 / 3, 35 % 21);
+		System.out.println("Power = " + pow);
+		double max2 = Math.max( Math.pow( 2,14%3) , Math.sqrt ( 2*6) );
+		System.out.println("Max = " + max2);
 	}
 }
